290401b137a483c5052241a7a2f6ed0a5cb99d87
Finn Terdal <fterdal@gmail.com>
2017-09-13 14:32:19 -0500

    Adds a reducer for classes

---
+++ client/store/classes.js
@@ -0,0 +1,40 @@
+import axios from 'axios'
+
+/**
+ * ACTION TYPES
+ */
+const GET_ALL_CLASSES = 'GET_ALL_CLASSES'
+
+/**
+ * INITIAL STATE
+ */
+const defaultClasses = []
+
+/**
+ * ACTION CREATORS
+ */
+const getAllClasses = classes => ({type: GET_ALL_CLASSES, classes })
+
+/**
+ * THUNK CREATORS
+ */
+export const fetchAllClasses = () => {
+  return dispatch => {
+    return axios.get('/classes')
+    .then(res =>
+      dispatch(getAllClasses(res.data || defaultClasses)))
+      .catch(err => console.log(err))
+  }
+}
+
+/**
+ * REDUCER
+ */
+export default function (classes = defaultClasses, action) {
+  switch (action.type) {
+    case GET_ALL_CLASSES:
+      return action.classes
+    default:
+      return classes
+  }
+}
--- client/store/index.js
+++ client/store/index.js
@@ -2,10 +2,16 @@ import {createStore, combineReducers, applyMiddleware} from 'redux'
 import createLogger from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import user from './user'
+import classes from './classes'
 
-const reducer = combineReducers({user})
+const reducer = combineReducers({
+  user,
+  classes,
+})
+
 const middleware = applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 const store = createStore(reducer, middleware)
 
 export default store
 export * from './user'
+export * from './classes'
