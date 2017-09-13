6c05c7bb527168099bb51705da48d2d6ac08d040
Tess Monical <tess.monical@gmail.com>
2017-09-13 16:04:08 -0500

    adds main.js changes to actually show navbar

--- client/components/main.js
+++ client/components/main.js
@@ -4,6 +4,7 @@ import {connect} from 'react-redux'
 import {withRouter, Link} from 'react-router-dom'
 import {logout} from '../store'
 // import ClassList from './class-list'
+import NavBar from './nav-bar';
 
 /**
  * COMPONENT
@@ -19,19 +20,23 @@ const Main = (props) => {
       <h1>BOILERMAKER</h1>
       <nav>
         {
-          isLoggedIn
-            ? <div>
-              {/* The navbar will show these links after you log in */}
-              <Link to='/home'>Home</Link>
-              <a href='#' onClick={handleClick}>Logout</a>
-            </div>
-            : <div>
-              {/* The navbar will show these links before you log in */}
-              <Link to='/login'>Login</Link>
-              <Link to='/signup'>Sign Up</Link>
-            </div>
+          // isLoggedIn
+          //   ? <div>
+          //     {/* The navbar will show these links after you log in */}
+          //     <Link to='/home'>Home</Link>
+          //     <a href='#' onClick={handleClick}>Logout</a>
+          //   </div>
+          //   : <div>
+          //     {/* The navbar will show these links before you log in */}
+          //     <Link to='/login'>Login</Link>
+          //     <Link to='/signup'>Sign Up</Link>
+          //   </div>
         }
       </nav>
+      <NavBar />
+
+
+
       <hr />
       {children}
     </div>
