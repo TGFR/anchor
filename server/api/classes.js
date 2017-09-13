f99c6d66eef2c44948d45d831085893ae36ee0fb
Greg <ajaxgreater@gmail.com>
2017-09-13 13:25:54 -0500

    classes and users routes

--- server/api/classes.js
+++ server/api/classes.js
@@ -8,3 +8,43 @@ router.get('/', (req, res, next) => {
     .then(classes => res.json(classes))
     .catch(next)
 })
+
+
+router.get('/:id', function (req, res, next) {
+  Class.findOne({where:{id: req.params.id}})
+    .then(classItem => res.json(classItem))
+    .catch(next);
+})
+
+router.post('/', function (req, res, next) {
+  console.log('trying to post')
+  Class.create(req.body)
+  .then(function(classItem) {
+    console.log('inthe post!');
+    res.status(201).json(classItem);
+  })
+  .catch(next);
+})
+
+
+router.put('/:id', function (req, res, next) {
+
+  Class.update(
+    req.body,
+    {where: {id: req.params.id}}
+  )
+  .then(function(classItem) {
+    res.json(classItem);
+  })
+  .catch(next);
+})
+
+
+router.delete('/:id', function (req, res, next) {
+  Class.destroy({where: {id: req.params.id}})
+  .then(function() {
+    res.json('success')
+  })
+  .catch(next)
+
+})
--- server/api/users.js
+++ server/api/users.js
@@ -12,3 +12,44 @@ router.get('/', (req, res, next) => {
     .then(users => res.json(users))
     .catch(next)
 })
+
+
+
+router.get('/:id', function (req, res, next) {
+  User.findOne({where:{id: req.params.id}})
+    .then(user => res.json(user))
+    .catch(next);
+})
+
+router.post('/', function (req, res, next) {
+  console.log('trying to post')
+  User.create(req.body)
+  .then(function(user) {
+    console.log('inthe post!');
+    res.status(201).json(user);
+  })
+  .catch(next);
+})
+
+
+router.put('/:id', function (req, res, next) {
+
+  User.update(
+    req.body,
+    {where: {id: req.params.id}}
+  )
+  .then(function(classItem) {
+    res.json(classItem);
+  })
+  .catch(next);
+})
+
+
+router.delete('/:id', function (req, res, next) {
+  User.destroy({where: {id: req.params.id}})
+  .then(function() {
+    res.json('success')
+  })
+  .catch(next)
+
+})
