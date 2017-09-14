dfb585fba4ff21f80f5ed3a98e4be761f47dfb93
Finn Terdal <fterdal@gmail.com>
2017-09-14 17:33:28 -0500

    Correctly renders orderItems in the accordian

--- client/components/user-orders.js
+++ client/components/user-orders.js
@@ -6,14 +6,28 @@ const UserOrders = (props) => {
   const {myOrders} = props;
   console.log('myOrders:', myOrders);
   const ordersAccordion = myOrders.map( order => {
+    const orderItems = order.orderItems.length ? order.orderItems.map( orderItem => {
+      return (
+        <Table.Row key={orderItem.id}>
+          <Table.Cell>{orderItem.id}</Table.Cell>
+          <Table.Cell>${orderItem.price}</Table.Cell>
+        </Table.Row>
+      )
+    }) : null;
     return {
       title: order.id,
-      content: 'hello',
+      content: (
+        <Table compact celled definition>
+          <Table.Body>
+            {orderItems}
+          </Table.Body>
+        </Table>
+      ),
     }
   })
   return (
     <div>
-      <Accordion panels={ordersAccordion} styled />
+      <Accordion panels={ordersAccordion} styled exclusive={false} />
     </div>
   )
 }
