5feaa25144e64c954fc988735cd8cd3793bfebaf
Finn Terdal <fterdal@gmail.com>
2017-09-14 10:41:40 -0500

    Add orders to api routes and adds orders (all of them) to the store

--- client/components/order-list.js
+++ client/components/order-list.js
@@ -2,28 +2,34 @@ import React from 'react';
 import { Item } from 'semantic-ui-react';
 import { connect } from 'react-redux';
 
-const OrderList = (props) => (
-  <Item.Group>
-    {
-      props.orders.map((order) => {
-        return (
-          <Item key = {order.id}>
-            <Item.Image size='tiny' src='/assets/images/wireframe/image.png' />
-            <Item.Content>
-              <Item.Header>{order.title}</Item.Header>
-              <Item.Meta>Date: {order.date}</Item.Meta>
-              <Item.Extra>Price: {order.subtotal}</Item.Extra>
-            </Item.Content>
-          </Item>
-        )
-      })
-    }
-  </Item.Group>
-);
+const OrderList = (props) => {
+  const {orders, users} = props;
+  console.log('users:', users);
+  return (
+    <Item.Group>
+      {
+        orders.map((order) => {
+          user = users.find(user => user.id === order.userId)
+          return (
+            <Item key = {order.id}>
+              <Item.Image size='tiny' src='/assets/images/wireframe/image.png' />
+              <Item.Content>
+                <Item.Header>User: {user.email}</Item.Header>
+                <Item.Meta>Date: {order.orderDate}</Item.Meta>
+                <Item.Extra>Price: {order.subtotal}</Item.Extra>
+              </Item.Content>
+            </Item>
+          )
+        })
+      }
+    </Item.Group>
+  )
+}
 
 const mapState = (state) => {
   return {
-    orders: state.orders
+    orders: state.orders,
+    users: state.users,
   }
 }
 
--- client/store/orders.js
+++ client/store/orders.js
@@ -1,9 +1,12 @@
+// NOTE This is the store for all users, and will only be filled in for admins
+// Look for the myOrders in the store for user-specific orders
 import axios from 'axios'
 
 /**
  * ACTION TYPES
  */
 const GET_ORDERS = 'GET_ORDERS'
+const CLEAR_ORDERS = 'CLEAR_ORDERS'
 
 /**
  * INITIAL STATE
@@ -13,14 +16,15 @@ const defaultOrders = []
 /**
  * ACTION CREATORS
  */
-const getOrders = orders => ({type: GET_ORDERS, orders })
+export const getOrders = orders => ({type: GET_ORDERS, orders })
+export const clearOrders = () => ({type: CLEAR_ORDERS })
 
 /**
  * THUNK CREATORS
  */
 export const fetchOrders = () => {
   return dispatch => {
-    return axios.get('/api/orders/user/:userid')
+    return axios.get('/api/orders')
     .then(res =>
       dispatch(getOrders(res.data || defaultOrders)))
       .catch(err => console.log(err))
@@ -34,6 +38,8 @@ export default function (orders = defaultOrders, action) {
   switch (action.type) {
     case GET_ORDERS:
       return action.orders
+    case CLEAR_ORDERS:
+      return []
     default:
       return orders
   }
