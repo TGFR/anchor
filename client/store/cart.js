dc7324613f059a18d3b591fc2c8b742f75fc03c8
Tess Monical <tess.monical@gmail.com>
2017-09-18 09:51:51 -0500

    added cases for adding and removing items to/from cart

--- client/store/cart.js
+++ client/store/cart.js
@@ -5,6 +5,8 @@ import axios from 'axios'
  */
 const GET_CART = 'GET_CART';
 const CLEAR_CART = 'CLEAR_CART';
+const ADD_CART_ITEM = 'ADD_CART_ITEM';
+const REMOVE_CART_ITEM = 'REMOVE_CART_ITEM';
 
 /**
  * INITIAL STATE
@@ -14,9 +16,10 @@ const defaultCart = []
 /**
  * ACTION CREATORS
  */
-export const getCart = cart => ({ type: GET_CART, cart })
-export const clearCart = () => ({ type: CLEAR_CART })
-
+export const getCart = cart => ({ type: GET_CART, cart });
+export const clearCart = () => ({ type: CLEAR_CART });
+export const addItem = item => ({ type: ADD_CART_ITEM, item });
+export const removeItem = itemId => ({ type: REMOVE_CART_ITEM, itemId });
 
 /**
  * THUNK CREATORS
@@ -30,15 +33,23 @@ export const fetchCart = () => {
   }
 }
 
+
 /**
  * REDUCER
  */
 export default function (cart = defaultCart, action) {
+  let newCart = [];
   switch (action.type) {
     case GET_CART:
       return action.cart;
     case CLEAR_CART:
       return defaultCart;
+    case ADD_CART_ITEM:
+      return [...cart, action.item];
+    case REMOVE_CART_ITEM:
+      let removeIndex = cart.find( (cartItem) => cartItem.id === action.itemId )
+        newCart = newCart.concat(cart.slice(0,removeIndex), cart.slice(removeIndex+1));
+        return newCart;
     default:
       return cart
   }
