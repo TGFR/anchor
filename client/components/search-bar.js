f678a5212fc3efa583979c2230fe22faef6736fc
Tess Monical <tess.monical@gmail.com>
2017-09-18 14:45:41 -0500

    factored search bar out of home.js

--- client/components/search-bar.js
+++ client/components/search-bar.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import { Container, Input } from 'semantic-ui-react'
 
-export default SearchBar => {
+const SearchBar = (props) => {
+
+  const size = props.size;
 
   return (
     <Container textAlign='center' className='search-container'>
-      <Input fluid size='medium' icon='search' placeholder='What are you looking to learn?' />
-    </Container>
+    <Input fluid size={size} icon='search' placeholder='What are you looking to learn?' />
+  </Container>
   )
 }
+
+export default SearchBar;
