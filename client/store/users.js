de6203c65be4ca952f1a88d37a17687847f806e6
Greg <ajaxgreater@gmail.com>
2017-09-19 17:39:55 -0500

    promote users works

--- client/store/users.js
+++ client/store/users.js
@@ -8,6 +8,7 @@ import { serverError } from './errors'
 const GET_ALL_USERS = 'GET_ALL_USERS'
 const CLEAR_USERS = 'CLEAR_USERS'
 const DELETE_USER = 'DELETE_USER'
+const PROMOTE_USER = 'PROMOTE_USER'
 
 /**
  * INITIAL STATE
@@ -20,6 +21,7 @@ const defaultUsers = []
 export const getUsers = users => ({type: GET_ALL_USERS, users})
 export const clearUsers = () => ({type: CLEAR_USERS})
 export const delUser = (id) => ({type: DELETE_USER, id})
+export const promUser = (user) => ({type: PROMOTE_USER, user})
 
 /**
  * THUNK CREATORS
@@ -35,6 +37,17 @@ export const fetchAllUsers = () => {
       })
   }
 
+}
+export const promoteUser = (user) => {
+  user.privilege = 'admin'
+  return dispatch => {
+    axios.put(`/api/users/${user.id}`, user)
+    .then((promotedUser) => dispatch(promUser(promotedUser)))
+    .catch(error => {
+      console.log(error)
+      dispatch(serverError(error))
+    })
+  }
 }
 
 export const deleteUser = (id) => {
@@ -59,7 +72,11 @@ export default function (users = defaultUsers, action) {
       return []
     case DELETE_USER:
       return users.filter(user => user.id !== action.id)
-
+    case PROMOTE_USER:
+      return users.map(user => {
+        if (user.id === action.user.id) return action.user
+        return user
+      })
     default:
       return users
   }
