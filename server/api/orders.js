41daae686be631fefb1c04763bd5543665555ae7
Finn Terdal <fterdal@gmail.com>
2017-09-18 21:23:39 -0500

    Async await AWAY!

--- server/api/orders.js
+++ server/api/orders.js
@@ -49,24 +49,33 @@ router.get('/:id', isSelfOrAdmin, function (req, res, next) {
     .catch(next);
 })
 
+async function validateOrderItems(cart) {
+  Object.keys(cart).map( async (classId) => {
+    const quantity = cart[classId];
+    const classItem = await Class.findById(classId)
+  })
+}
+
 //make a new order
 // TODO Make sure the user id associated with the new order
 // matches that of the requesting user. Otherwise, some hacker
 // with Postman can just make an order associated with someone else
 router.post('/', function (req, res, next) {
-  /* CASES
-  1. User is logged in
-      -> create new order with that userId
-      -> create an orderitem for each key in the cart
-      -> set the price equal to the price of that class
-      -> associate each of these orderitems with that new order
-  2. User doesn't exist (provides email address)
-      -> Create user...
-  3. User isn't logged in, but email matches existing user
-      -> ...
-  4. Cart is empty
-      -> Don't create an order in the first place. Return 400.
-  */
+  /* Comments */ {
+    /* CASES
+    1. User is logged in
+    -> create new order with that userId
+    -> create an orderitem for each key in the cart
+    -> set the price equal to the price of that class
+    -> associate each of these orderitems with that new order
+    2. User doesn't exist (provides email address)
+    -> Create user...
+    3. User isn't logged in, but email matches existing user
+    -> ...
+    4. Cart is empty
+    -> Don't create an order in the first place. Return 400.
+    */
+  }
 
   const userId = req.user.id;
   const cart = req.session.cart;
